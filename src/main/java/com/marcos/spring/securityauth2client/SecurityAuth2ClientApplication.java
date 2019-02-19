c0a3aaf76514cec531de57e32c95c3e85afa9fbf
Marcos Gui <marcos.gui@spsoft-cn.com>
2019-02-19 17:47:28 +0800

    [INIT] First commit

---
+++ src/main/java/com/marcos/spring/securityauth2client/SecurityAuth2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.spring.securityauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAuth2ClientApplication{
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecurityAuth2ClientApplication.class, args);
+  }
+
+}
